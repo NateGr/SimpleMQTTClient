943b0182b6304c2acacc081ddf928aaff38d5850
Nate <nathan.gropman@gmail.com>
2024-12-12 21:56:39 -0700

    Made a command line params

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Text;
 using MQTTnet;
 using MQTTnet.Client;
 using System.Threading.Tasks;
-using MQTTnet.Diagnostics;
 
 namespace MyMqttApp
 {
@@ -11,25 +10,38 @@ namespace MyMqttApp
     {
         static async Task Main(string[] args)
         {
+            if (args.Length < 5)
+            {
+                Console.WriteLine("Usage: MyMqttApp <username> <password> <host> <topic> <message>");
+                return;
+            }
+
+            string username = args[0];
+            string password = args[1];
+            string host = args[2];
+            string topic = args[3];
+            string message = args[4];
+
             var factory = new MqttFactory();
             var mqttClient = factory.CreateMqttClient();
             var options = new MqttClientOptionsBuilder()
-                .WithTcpServer("homeassistant.local")
-                .WithCredentials("mqttuserext", "Starbug1!")
+                .WithTcpServer(host)
+                .WithCredentials(username, password)
                 .Build();
 
             await mqttClient.ConnectAsync(options);
 
-            var message = new MqttApplicationMessageBuilder()
-                .WithTopic("homeassistant/customaction/execute")
-                .WithPayload("toggle_light|light.master_bedroom_lamp")
+            var mqttMessage = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(message)
                 .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce)
                 .Build();
 
-
-            await mqttClient.PublishAsync(message);
+            await mqttClient.PublishAsync(mqttMessage);
 
             await mqttClient.DisconnectAsync();
+
+            Console.WriteLine("Message published successfully!");
         }
     }
 }
