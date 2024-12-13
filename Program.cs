241246812bd8a35506328002557dc9bfd77fa934
Nate <nathan.gropman@gmail.com>
2024-12-12 21:54:12 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Threading.Tasks;
+using MQTTnet.Diagnostics;
+
+namespace MyMqttApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("homeassistant.local")
+                .WithCredentials("mqttuserext", "Starbug1!")
+                .Build();
+
+            await mqttClient.ConnectAsync(options);
+
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("homeassistant/customaction/execute")
+                .WithPayload("toggle_light|light.master_bedroom_lamp")
+                .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce)
+                .Build();
+
+
+            await mqttClient.PublishAsync(message);
+
+            await mqttClient.DisconnectAsync();
+        }
+    }
+}
